c6ce305c8202b157eb3ecae8ed6fac8ee28db91f
Guillaume <siberiankebab@gmail.com>
2022-07-24 14:43:38 +0400

    first commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,25 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+            details: {
+              orderLiterals: true,
+              deduplicate: true,
+              cse: true,
+              yul: true,
+            },
+          },
+        },
+      },
+    ],
+  }
 };
 
 export default config;
